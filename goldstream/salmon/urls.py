44f0aca92c7f82717f97a32158f2de15bd7f4521
Alan Manning <alanmanning@gmail.com>
2017-09-06 17:55:40 -0700

    Second commit

--- goldstream/salmon/urls.py
+++ goldstream/salmon/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^login', views.login, name='login'),
 	url(r'^register', views.register, name='register'),
+	url(r'^show_users',views.ShowUsers.as_view(), name='show_users'),
 
 	]
--- goldstream/salmon/views.py
+++ goldstream/salmon/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import RegisterForm
+from django.views import generic
+from .models import User
 
 # Create your views here.
 def index(request):
@@ -12,10 +14,17 @@ def login(request):
 def register(request):
 	if request.method == 'POST':
 		form = RegisterForm(request.POST)
-		if form.is_valid()
+		if form.is_valid():
 			form.save()
-			return redirect('salmon:user_list')
+			return redirect('salmon:show_users')
 
 	else:
-	form = RegisterForm()
+		form = RegisterForm()
 	return render(request,'salmon/register.html', {'form':form})
+
+class ShowUsers(generic.ListView):
+	template_name = 'salmon/show_users.html'
+	context_object_name = 'user_list'
+
+	def get_queryset(self):
+		return User.objects.all()

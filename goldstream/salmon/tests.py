9937ff1c47509e343473fe4524cb1e61f072d56b
alan <alan@reven>
2017-11-13 15:57:35 -0800

    Starting work on this again.

--- goldstream/salmon/tests.py
+++ goldstream/salmon/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.urls import resolve
+#from lists.views import home_page
+#from lists.models import Item, List
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.db.utils import IntegrityError
+#from pdb import set_trace
+import random
+
+# Create your tests here.
